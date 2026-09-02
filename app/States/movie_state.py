f0ab57dcfecb70d4d2845429c2c1188e8d704294
Daminov3 <daminovabdulaziz4@gmail.com>
2026-09-02 15:20:09 +0500

    Add files via upload

--- app/States/movie_state.py
+++ app/States/movie_state.py
@@ -1,1 +1,10 @@
+from aiogram.fsm.state import StatesGroup, State
 
+
+class MovieState(StatesGroup):
+    waiting_code = State()
+    waiting_title = State()
+    waiting_video = State()
+
+    # Backup import
+    waiting_backup = State()
