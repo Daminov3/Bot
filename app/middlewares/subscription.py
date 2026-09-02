727086affc37efcd4a7fdaf5f9c37fe5c41cd57a
Daminov3 <daminovabdulaziz4@gmail.com>
2026-09-02 15:22:15 +0500

    Add files via upload

--- app/middlewares/subscription.py
+++ app/middlewares/subscription.py
@@ -1,1 +1,44 @@
+from app.database import get_all_channels
 
+
+async def get_unsubscribed_channels(bot, user_id):
+
+    channels = get_all_channels()
+
+    # Kanal bo'lmasa
+    if not channels:
+        return []
+
+    unsubscribed = []
+
+    for channel in channels:
+
+        try:
+            member = await bot.get_chat_member(
+                chat_id=channel,
+                user_id=user_id
+            )
+
+            if member.status not in [
+                "member",
+                "administrator",
+                "creator"
+            ]:
+                unsubscribed.append(channel)
+
+        except Exception:
+            # Agar bot kanalni tekshira olmasa
+            unsubscribed.append(channel)
+
+    return unsubscribed
+
+
+
+async def check_subscription(bot, user_id):
+
+    unsubscribed = await get_unsubscribed_channels(
+        bot,
+        user_id
+    )
+
+    return len(unsubscribed) == 0
